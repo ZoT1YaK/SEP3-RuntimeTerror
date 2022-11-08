89ed572afd44adfbe840ffe22ddcc0bfe77d47bf
Bl1tzz18 <91396657+Bl1tzz18@users.noreply.github.com>
2022-11-08 21:45:55 +0100

    God Help us

---
+++ PresentationTier/HttpClients/ClientImpl/UserHttpClient.cs
@@ -0,0 +1,52 @@
+using System.Net.Http.Json;
+using System.Security.Claims;
+using System.Text.Json;
+using HttpClients.ClientInterfaces;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace HttpClients.ClientImpl;
+
+public class UserHttpClient
+{
+    private readonly HttpClient Client;
+    public static string? Jwt { get; private set; } = "";
+    
+    public UserHttpClient(HttpClient client)
+    {
+        Client = client;
+    }
+    
+    public async Task<User> Create(UserCreationDTO dto)
+    {
+        HttpResponseMessage response = await Client.PostAsJsonAsync("/users", dto);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+
+        User user = JsonSerializer.Deserialize<User>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return user;
+    }
+
+    public Task LoginAsync(string username, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task LogoutAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ClaimsPrincipal> GetAuthAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
+}
---
+++ PresentationTier/HttpClients/ClientInterfaces/IUserService.cs
@@ -0,0 +1,16 @@
+using System.Security.Claims;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IUserService
+{
+    Task<User> Create(UserCreationDTO dto);
+    
+    public Task LoginAsync(string username, string password);
+    public Task LogoutAsync();
+    
+    public Task<ClaimsPrincipal> GetAuthAsync();
+    public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
+}
