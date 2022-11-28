b14976262aed41391186b75a43d98a80c0a55329
GluhaiaMuha <57876096+GluhaiaMuha@users.noreply.github.com>
2022-11-28 22:46:03 +0100

    Presentation Tier Upgrade

--- PresentationTier/BlazorApp/Program.cs
+++ PresentationTier/BlazorApp/Program.cs
@@ -14,4 +14,5 @@ builder.Services.AddScoped(sp => new System.Net.Http.HttpClient { BaseAddress = 
 
 builder.Services.AddScoped<IUserService, UserHttpClient>();
 
+
 await builder.Build().RunAsync();
